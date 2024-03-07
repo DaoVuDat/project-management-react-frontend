84fade6117dc44d6351b652eca2ef111795d9b8f
Đào Vũ Đạt <dat.daovu@gmail.com>
2024-03-08 01:05:04 +0700

    hide router devtool in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,31 @@
 import {createRootRouteWithContext, Outlet} from '@tanstack/react-router';
-import {TanStackRouterDevtools} from '@tanstack/router-devtools';
 import {QueryClient} from '@tanstack/react-query';
+import React, {Suspense} from 'react';
 
 
 interface RootRouteContext {
   queryClient: QueryClient;
 }
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    )
+
 export const Route = createRootRouteWithContext<RootRouteContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
   notFoundComponent: () => 'Page Not Found',
