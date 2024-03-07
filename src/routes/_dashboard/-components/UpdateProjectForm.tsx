a3c694d5299c38f89dccb0be99cfb8e216106ace
Đào Vũ Đạt <dat.daovu@gmail.com>
2024-03-07 11:51:40 +0700

    fix UpdateProjectForm initialValue Cost

--- src/routes/_dashboard/-components/UpdateProjectForm.tsx
+++ src/routes/_dashboard/-components/UpdateProjectForm.tsx
@@ -184,6 +184,7 @@ export function UpdateProjectForm({onClose, project}: UpdateProjectFormProps) {
 					</label>
 					{/*<input type='number' {...register('price')} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-secondary sm:text-sm sm:leading-6'/>*/}
 					<CommaInput
+						initialValue={(project.price * 1_000_000).toString()}
 						name="price"
 						control={control}
 					/>
